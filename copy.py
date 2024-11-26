ce235320beb634ef314af79088f933dbe9ced1e4
Hchaymae <hamdounechaymae@gmail.com>
2024-11-26 14:34:12 -0800

    Adding Endpoint

--- copy.py
+++ copy.py
@@ -1,10 +1,10 @@
 import shutil
 
 # Path to the original notebook
-original_notebook_path = "model_training.ipynb"
+original_notebook_path = "notebooks/feature_selection.ipynb"
 
 # Path to the new notebook (can be a different directory or name)
-new_notebook_path = "notebooks/model_training.ipynb"
+new_notebook_path = "model_training.ipynb"
 
 # Copy the notebook
 shutil.copy(original_notebook_path, new_notebook_path)
---
+++ src/loan_classifier/app/utils.py
@@ -0,0 +1,3 @@
+from keras.models import load_model
+
+model = load_model("../ml/100k_ann_model.keras")
