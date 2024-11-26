468dcf20d657704e20be7c153f94f9444ebeb994
Hchaymae <hamdounechaymae@gmail.com>
2024-11-26 16:34:56 +0100

    Fixing

--- src/loan_classifier/app/utils.py
+++ src/loan_classifier/app/utils.py
@@ -1,3 +1,5 @@
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
-model = load_model("../ml/100k_ann_model.keras")
+#model = load_model("../ml/100k_ann_model.keras")
+model = load_model(r"C:\Users\chaym\OneDrive - Université Cadi Ayyad Marrakech\Documents\CI5\INTELLIGENCE ARTIFICIELLE\Credit_eligibility_AI_recommendation_system\src\loan_classifier\ml\100k_ann_model.keras")
+print("Model loaded successfully:", model)
