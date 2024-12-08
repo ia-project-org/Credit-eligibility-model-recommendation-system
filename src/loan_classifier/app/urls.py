cffe336eb0388812f3851fa54342bebfda3a8a8e
Hamdoune Chaymae <89420843+Hchaymae@users.noreply.github.com>
2024-12-08 01:16:27 +0100

    Delete src directory

--- src/loan_classifier/app/urls.py
+++
@@ -1,7 +0,0 @@
-# app/urls.py
-from django.urls import path
-from .views import predict_credit_score
-
-urlpatterns = [
-    path('predict/', predict_credit_score, name='predict_credit_score'),
-]
--- src/loan_classifier/app/utils.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-import joblib
-
-
-
-scaler = joblib.load(r'C:\Users\chaym\OneDrive - Université Cadi Ayyad Marrakech\Documents\CI5\INTELLIGENCE ARTIFICIELLE\Credit_eligibility_AI_recommendation_system\src\scaler.pkl')
-model = load_model(r"C:\Users\chaym\OneDrive - Université Cadi Ayyad Marrakech\Documents\CI5\INTELLIGENCE ARTIFICIELLE\Credit_eligibility_AI_recommendation_system\src\100k_ann_model.keras")
-print("Model loaded successfully:", model)
-
